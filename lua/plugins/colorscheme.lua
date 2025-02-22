13d6dadd7dbd1d5bd11f02ed61f17e4989a67810
aanes1 <anesen.niklas@gmail.com>
2025-02-22 13:15:36 +0100

    fixed some colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,5 +4,26 @@ return {
 	priority = 1000,
 	init = function()
 		vim.cmd.colorscheme("sakura")
+		local purple = "#a289a1"
+
+		local highlights = {
+			--general
+			ModeMsg = { fg = purple },
+			CursorLineNr = { fg = purple },
+
+			-- git signs
+			GitSignsAdd = { fg = purple },
+			GitSignsAddNr = { fg = purple },
+			GitSignsAddLn = { fg = purple },
+			GitSignsChange = { fg = purple },
+			GitSignsChangeNr = { fg = purple },
+			GitSignsChangeLn = { fg = purple },
+			GitSignsChangedelete = { fg = purple },
+		}
+
+		-- set highlight colors
+		for group, colors in pairs(highlights) do
+			vim.api.nvim_set_hl(0, group, colors)
+		end
 	end,
 }
