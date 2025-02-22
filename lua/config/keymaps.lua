29c2945c54644c5f7415d02194f44b26233d5903
aanes1 <anesen.niklas@gmail.com>
2025-02-22 22:53:20 +0100

    toggleterm fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,4 @@ local keymap = vim.keymap
 
 keymap.set("n", "<leader>rs", ":LspRestart<CR>", { desc = "Restart LSP" })
 keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+keymap.set("t", "<esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
