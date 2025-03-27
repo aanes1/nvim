d07e128e07791efa274cfae33a258e8c8cffdcf5
aanes1 <anesen.niklas@gmail.com>
2025-03-27 12:44:26 +0100

    add keymaps for navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,8 @@ keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file ex
 keymap.set("t", "<esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 keymap.set("n", "gt", ":BufferLineCycleNext<CR>", { desc = "Goto next buffer" })
 keymap.set("n", "gT", ":BufferLineCyclePrev<CR>", { desc = "Goto prev buffer" })
+
+keymap.set("t", "<C-h>", "<C-\\><C-n><C-h>", { desc = "Navigate left" })
+keymap.set("t", "<C-j>", "<C-\\><C-n><C-j>", { desc = "Navigate down" })
+keymap.set("t", "<C-k>", "<C-\\><C-n><C-k>", { desc = "Navigate up" })
+keymap.set("t", "<C-l>", "<C-\\><C-n><C-l>", { desc = "Navigate right" })
